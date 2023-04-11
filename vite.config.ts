c57fa6e9f765c17fb3579a6c598f6ef7533c1b64
kkkkoki <koki_inoue45@icloud.com>
2023-04-11 20:32:47 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? "react-dark-theme-demo" : "./",
+  base: "/react-dark-theme-demo/",
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
