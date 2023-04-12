af3cceb302a879ef7070ccd51fe5c8f583a415e1
kkkkoki <koki_inoue45@icloud.com>
2023-04-13 07:17:53 +0900

    fix animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,29 @@
 import "./App.css";
-import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
-import { css } from "@emotion/react";
-import { useState } from "react";
+import { ThemeProvider } from "@emotion/react";
+import EmotionAnimationButton from "./components/EmotionAnimationButton";
+
+const theme = {
+  light: {
+    mode: "ligth",
+    background: "#f8f8f8",
+    color: "#000000",
+  },
+
+  dark: {
+    mode: "dark",
+    background: "#2b2b2b",
+    color: "#f0f5fa",
+  },
+};
 
 function App() {
-  const [checked, setChecked] = useState(true);
-  const classes = {
-    label: css({
-      display: "grid",
-      placeItems: "center",
-      justifyContent: "space-around",
-      width: 80,
-      height: 80,
-      borderRadius: "50%",
-      cursor: "pointer",
-    }),
-
-    checkBox: css({
-      position: "absolute",
-      opacity: 0,
-      cursor: "pointer",
-    }),
-
-    sun: css({
-      position: "absolute",
-      width: 40,
-      height: 40,
-      color: "#666",
-      filter: "drop-shadow(0 0 2px rgba(0, 0, 0, .5))",
-      transform: checked ? "scale(1) rotate(360deg)" : "scale(0)",
-      transition: ".5s ease",
-      transitionDelay: checked ? ".5s" : "0",
-    }),
-
-    moon: css({
-      width: 40,
-      height: 40,
-      color: "#666",
-      filter: "drop-shadow(0 0 2px rgba(0, 0, 0, .5))",
-      transform: checked ? "rotate(360deg) scale(0)" : "none",
-      transition: ".5s ease",
-      transitionDelay: checked ? "0" : ".5s",
-    }),
-
-    toggle: css({
-      position: "absolute",
-      display: "block",
-      width: 80,
-      height: 80,
-      background: checked ? "#f8f8f8" : "#2b2b2b",
-      borderRadius: "50%",
-      boxShadow:
-        "inset 0 8px 60px rgba(0, 0, 0, .1), inset 0 8px 8px rgba(0, 0, 0, .1), inset 0 -4px 4px rgba(0, 0, 0, .1)",
-      zIndex: -1,
-      transition: ".5s",
-    }),
-
-    animateBackGround: css({
-      position: "absolute",
-      width: "100%",
-      height: "100vh",
-      background: "#f8f8f8",
-      zIndex: -2,
-      clipPath: checked ? "circle(100% at 50% 50%)" : "circle(0% at 50% 50%)",
-      transition: "1.5s ease-out",
-    }),
-  };
+  const isDark = localStorage.getItem("theme") === "dark";
+  console.log(isDark);
 
   return (
-    <>
-      <label css={classes.label}>
-        <input
-          css={classes.checkBox}
-          type="checkbox"
-          onClick={() => setChecked(!checked)}
-        />
-        <MoonIcon css={classes.moon} />
-        <SunIcon css={classes.sun} />
-        <span css={classes.toggle}></span>
-        <span css={classes.animateBackGround}></span>
-      </label>
-    </>
+    <ThemeProvider theme={isDark ? theme.dark : theme.light}>
+      <EmotionAnimationButton />
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/EmotionAnimationButton.tsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
+import { css, useTheme } from "@emotion/react";
+
+const EmotionAnimationButton = () => {
+  const theme = useTheme();
+  const { mode }: any = { ...theme };
+
+  const isDark = mode === "dark";
+  const [checked, setChecked] = useState(isDark);
+
+  const classes = {
+    label: css({
+      position: "fixed",
+      top: 0,
+      right: 0,
+      display: "inline-flex",
+      alignItems: "center",
+      justifyContent: "center",
+      width: 40,
+      height: 40,
+      borderRadius: "50%",
+      cursor: "pointer",
+    }),
+
+    checkBox: css({
+      position: "absolute",
+      opacity: 0,
+      cursor: "pointer",
+    }),
+
+    icon: css({
+      width: "100%",
+      height: "100%",
+    }),
+
+    sun: css({
+      position: "absolute",
+      color: "orange",
+      filter: "drop-shadow(0 0 2px rgba(0, 0, 0, .5))",
+      transform: checked ? "scale(1) rotate(360deg)" : "scale(0)",
+      transition: ".5s ease",
+      transitionDelay: checked ? ".5s" : "0",
+    }),
+
+    moon: css({
+      filter: "drop-shadow(0 0 2px rgba(0, 0, 0, .5))",
+      color: "skyblue",
+      transform: checked ? "rotate(360deg) scale(0)" : "none",
+      transition: ".5s ease",
+      transitionDelay: checked ? "0" : ".5s",
+    }),
+
+    animateBackGround: css({
+      position: "fixed",
+      width: "500vmax",
+      height: "500vmax",
+      background: "#f8f8f8",
+      zIndex: -2,
+      clipPath: checked ? "circle(100% at center)" : "circle(0% at center)",
+      transition: "1.5s ease-out",
+      pointerEvents: "none",
+    }),
+  };
+
+  const setTheme = () => {
+    localStorage.setItem("theme", isDark ? "ligth" : "dark");
+  };
+
+  return (
+    <>
+      <label css={classes.label}>
+        <input
+          css={classes.checkBox}
+          type="checkbox"
+          onClick={() => {
+            setChecked(!checked);
+            setTheme();
+          }}
+        />
+        <MoonIcon css={[classes.icon, classes.moon]} />
+        <SunIcon css={[classes.icon, classes.sun]} />
+        <span css={classes.animateBackGround}></span>
+      </label>
+    </>
+  );
+};
+
+export default EmotionAnimationButton;
