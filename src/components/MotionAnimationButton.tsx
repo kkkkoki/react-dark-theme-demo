193c15e3cff648d3509ac6838ea349dc1b293739
kkkkoki <koki_inoue45@icloud.com>
2023-04-15 05:51:07 +0900

    fix 初回レンダリングでアニメーションしないように修正

--- src/components/MotionAnimationButton.tsx
+++ src/components/MotionAnimationButton.tsx
@@ -13,8 +13,16 @@ const MotionAnimationButton: React.FC<ThemeToggleButtonProps> = ({
   isDark,
 }) => {
   const [dark, setDark] = useState(isDark);
+  const [isInit, setIsInit] = useState(true);
 
   const classes = {
+    button: css({
+      width: 80,
+      height: 80,
+      background: "#242424",
+      border: "none",
+    }),
+
     icon: css({
       width: "100%",
       height: "100%",
@@ -31,40 +39,58 @@ const MotionAnimationButton: React.FC<ThemeToggleButtonProps> = ({
   };
 
   const handle = () => {
+    if (isInit) {
+      setIsInit(false);
+    }
+
     setDark(!dark);
     localStorage.setItem("theme", dark ? "ligth" : "dark");
   };
 
   const animationVariants: Variants = {
-    initial: { scale: 0 },
-    animate: { rotate: "360deg", scale: 1, transition: { duration: 0.5 } },
-    exit: { rotate: "720deg", scale: 0, transition: { duration: 0.5 } },
+    initial: { scale: isInit ? 1 : 0 },
+
+    animate: {
+      rotate: `${dark ? "360deg" : "-360deg"}`,
+      scale: 1,
+      transition: { duration: 0.5 },
+    },
+
+    exit: {
+      rotate: 0,
+      scale: 0,
+      transition: { duration: 0.5 },
+    },
   };
 
   return (
-    <button onClick={() => handle()}>
-      <AnimatePresence mode="wait">
-        {dark ? (
-          <motion.div
-            key="sun"
-            initial="initial"
-            animate="animate"
-            exit="exit"
-            variants={animationVariants}>
-            <MoonIcon css={[classes.icon, classes.moon]} />
-          </motion.div>
-        ) : (
-          <motion.div
-            key="moon"
-            initial="initial"
-            animate="animate"
-            exit="exit"
-            variants={animationVariants}>
-            <SunIcon css={[classes.icon, classes.sun]} />
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </button>
+    <AnimatePresence mode="wait">
+      {dark ? (
+        <motion.button
+          css={classes.button}
+          onClick={() => handle()}
+          key="sun"
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          style={{ rotate: isInit ? "360deg" : 0 }}
+          variants={animationVariants}>
+          <MoonIcon css={[classes.icon, classes.moon]} />
+        </motion.button>
+      ) : (
+        <motion.button
+          css={classes.button}
+          onClick={() => handle()}
+          key="moon"
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          style={{ rotate: isInit ? "-360deg" : 0 }}
+          variants={animationVariants}>
+          <SunIcon css={[classes.icon, classes.sun]} />
+        </motion.button>
+      )}
+    </AnimatePresence>
   );
 };
 
